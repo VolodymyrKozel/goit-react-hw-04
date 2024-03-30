617ed4abdc79081d58d10d7dda36b24993ce75b9
Volodymyr <Volodumurvel@gmail.com>
2024-03-30 08:14:09 +0100

    error

--- src/components/errorMessage/ErrorMessage.jsx
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -1,6 +1,6 @@
 import css from './ErrorMessage.module.css';
 
-export default function ErrorMessage({ error }) {
+function ErrorMessage({ error }) {
   return (
     <div className={css.container}>
       <h1 className={css.heading}>
@@ -12,3 +12,4 @@ export default function ErrorMessage({ error }) {
     </div>
   );
 }
+export default ErrorMessage;
